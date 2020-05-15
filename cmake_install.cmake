182794975d24b87b38250591f05badbaf95b884e
Michal Dacko <dackomichal@gmail.com>
2020-05-16 00:23:24 +0100

    Switched out simple web server for cpphttplib

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -42,11 +42,6 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/home/dachckol/cpp/michal_vs_computers/discount/cmake/cmake_install.cmake")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/dachckol/cpp/michal_vs_computers/Simple-Web-Server/cmake_install.cmake")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
